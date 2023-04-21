71ee7ea43a6bbb19c32d349bd78a678a58d0481f
mohamadch91 <mohamadchoupan80@gmail.com>
2023-04-21 13:17:47 +0330

    refactor

--- HW1/task.py
+++ HW1/task.py
@@ -10,8 +10,19 @@ SPORADIC  =3  # Task type is sporadic
 
 
 class Task (object):
-    
-    def __init__(self,priority=255,name=None,state=SUSPENDED,type=None,act_time=0,period=0,wcet=0) ->None:
+    """Task Object Class
+
+    Attributes:
+        priority (int): Priority of the task
+        name (str): Name of the task
+        state (int): State of the task
+        type (int): Type of the task
+        act_time (int): Activation time of the task
+        period (int): Period of the task
+        wcet (int): Worst case execution time of the task
+        deadline (int): Deadline of the task
+    """
+    def __init__(self,priority=255,name=None,state=SUSPENDED,type=None,act_time=0,period=0,wcet=0,deadline=1000):
         self.priority = priority
         self.name = name
         self.state = state
@@ -19,13 +30,25 @@ class Task (object):
         self.act_time = act_time
         self.period = period
         self.wcet = wcet
+        self.deadline = deadline
+
         # you can add more attributes if you need
         
     
-    def change_state(self,state):
+    def change_state(self,state) -> None:
+        """Change the state of the task
+
+        Args:
+            state (int): New state of the task
+        """
         self.state = state
 
-    def change_priority(self,priority):
+    def change_priority(self,priority) -> None:
+        """Change the priority of the task
+
+        Args:
+            priority (int): New priority of the task
+        """
         self.priority = priority
         
     
